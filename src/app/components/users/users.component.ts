e4051804804184ba274ccb364ea7d4a6f7514c95
Ako <akaki.vashakidze1@gmail.com>
2025-11-01 00:20:25 +0400

    users list

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { SharedService } from '../../services/shared.service';
 import { User } from '../../interfaces/users.interface';
 import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   standalone: true,
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
 })
 export class UsersComponent {
   users$: Observable<User[]>;
+  dataSource = new MatTableDataSource<User>();
+
+  displayedColumns: string[] = ['name', 'username', 'phone', 'email', 'company', 'posts'];
 
   constructor(private sharedService: SharedService) {
     this.users$ = this.sharedService.getUsers();
+
+    this.users$.subscribe(users => {
+      this.dataSource.data = users;
+    });
+  }
+
+  onActionClick(userId: number) {
+    console.log('User ID clicked:', userId);
   }
 }
