adbc302c13cc6f20412aef7eac6d213d18fbbac2
Ako <akaki.vashakidze1@gmail.com>
2025-10-31 21:51:15 +0400

    adding side nav

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [CommonModule, MatIconModule, RouterLink],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
