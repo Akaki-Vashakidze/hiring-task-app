4fcf05a3ff653bc9efe94a87901fdcf1d809346a
Ako <akaki.vashakidze1@gmail.com>
2025-10-31 19:27:31 +0400

    change material color theme, style header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { interval, startWith, map } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule,MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  
+  currentDateTime!: Date; 
+  ngOnInit() {
+    interval(1000).pipe(
+      startWith(0),
+      map(() => new Date())
+    ).subscribe(date => {
+      this.currentDateTime = date;
+    });
+  }
 }
