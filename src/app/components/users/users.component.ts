c98221365b062ef94229fffcc5432f1ff74f8df3
Ako <akaki.vashakidze1@gmail.com>
2025-11-02 00:03:34 +0400

    users page finished

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -4,32 +4,47 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { SharedService } from '../../services/shared.service';
-import { User } from '../../interfaces/users.interface';
-import { Observable } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
+import { User } from '../../interfaces/users.interface';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   standalone: true,
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
-  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule, MatPaginatorModule, MatInputModule, MatFormFieldModule],
 })
 export class UsersComponent {
-  users$: Observable<User[]>;
-  dataSource = new MatTableDataSource<User>();
-
-  displayedColumns: string[] = ['name', 'username', 'phone', 'email', 'company', 'posts'];
-
-  constructor(private sharedService: SharedService) {
-    this.users$ = this.sharedService.getUsers();
-
-    this.users$.subscribe(users => {
-      this.dataSource.data = users;
+  displayedColumns = ['name', 'username', 'phone', 'email' , 'company', 'posts'];
+  dataSource!: MatTableDataSource<User>;
+  name = 'Angular 5';
+  options = {
+    timeOut: 3000,
+    showProgressBar: true,
+    pauseOnHover: true,
+    clickToClose: true
+  };
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private _sharedService: SharedService) {
+    this._sharedService.getUsers().subscribe(users => {
+      this.dataSource = new MatTableDataSource(users);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
 
-  onActionClick(userId: number) {
-    console.log('User ID clicked:', userId);
+  viewPosts(user: User) {
+    console.log(user)
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
