25b4a66419cbd90254740bd7327f5a7bdc3cc99a
Ako <akaki.vashakidze1@gmail.com>
2025-11-03 13:55:13 +0400

    minor changes - hide content untill http responce, footer display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
 import { LoaderComponent } from "./components/spinner/spinner.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoaderComponent, HeaderComponent, NavigationComponent, NavigationComponent, LoaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LoaderComponent, HeaderComponent, FooterComponent,NavigationComponent, NavigationComponent, LoaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
